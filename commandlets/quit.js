85d764dcacd00bbbd58373c31416c178319d5a87
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-11-10 16:43:09 -0800

    silly fun

--- commandlets/quit.js
+++ commandlets/quit.js
@@ -1,6 +1,22 @@
 'use strict';
 
 module.exports = function(context, callback) {
-    console.log('Goodbye\n');
+    var cowsay = require('cowsay');
+    var faces = [
+        'default',
+        'squirrel',
+        'turtle',
+        'moose',
+        'dragon',
+        'dragon-and-cow'
+    ];
+
+    var randomFace = faces[Math.floor(Math.random()*faces.length)];
+    console.log(cowsay.say({
+        text:'Goooodbye',
+        f: randomFace,
+        e: 'oO',
+        T: 'U '
+    }));
     process.exit();
 };
